82f5430de4b19d98f288eac44b51e4524651827b
Slfdspln <121422214+Slfdspln@users.noreply.github.com>
2023-07-08 20:56:44 -0700

    added component files and associated page files, added sample text to…

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState("About");
+  return (
+    <div>
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <PortfolioContainer
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <Footer />
+    </div>
+  );
+};
+
+
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <PortfolioContainer />
+//       </div>
+//       <div className="App">
+//         <header className="App-header">
+//           {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//           <p>Welcome to my portfolio!</p>
+//           <OrangeBoxes />
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     </>
+//   );
+// }
+
+export default App;
